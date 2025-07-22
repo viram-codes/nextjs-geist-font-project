672d713dd3e1e697504eec714e86e725c9cf72b4
viram-codes <viramwrites@gmail.com>
2025-07-22 14:03:41 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753173220357

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_5f204215._.js",
+  "static/chunks/node_modules_df59aea9._.js"
+],
+    source: "dynamic"
+});
